74175c496466961a59ab2f7355e04f25be588b92
Adrian Udovicic <adrian.udovicic@outlook.com>
2020-06-08 23:26:16 +0200

    Testiram kod i pokušavam skužit zašto mi se "Git failed with a fatal error. error: open(".vs/Pi2Test/v16/Browse.VC.opendb"): Permission denied fatal: Unable to process path .vs/Pi2Test/v16/Browse.VC.opendb"  javlja.

--- Pi2Test.cpp
+++ Pi2Test.cpp
@@ -1,22 +1,38 @@
-// Pi2Test.cpp : This file contains the 'main' function. Program execution begins and ends there.
+﻿// Pi2Test.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
 #include <iostream>
-#include "TCanvas.h"
+#include <random>
+#include <math.h>
+#include <time.h>
 
+using namespace std;
 
+/*
+	Treba napraviti MCSim za generiranje broja \pi. 
+	U kvadratu površine 1 (1x1) generirati će se parovi brojeva (x,y)&[0-1].
+	Upisati kružnicu radiusa 1, površine 1 * 1 * \pi= \pi. Prebrojati koliko se točaka nalazi u kružnici.
+	BrTuKrug/BrTuKvad=\pi/4
+*/
 int main()
 {
-    std::cout << "Hello World!\n";
+	srand(time(NULL));
+	double rand_x, rand_y;
+	double polozaj;
+	int BrTuKrug = 0, BrTuKvad=100000;
+	
+
+	for (int i = 0; i < BrTuKvad; i++)
+	{
+		rand_x = ((double)rand() / (RAND_MAX));
+		rand_y = ((double)rand() / (RAND_MAX));
+		polozaj = pow(rand_x, 2) + pow(rand_y, 2);
+		if (sqrt(polozaj) <= 1)
+			BrTuKrug++;
+		/*cout << "rand_x= " << rand_x <<"rand_y= "<< rand_y<< endl;*/
+	}
+
+	cout << BrTuKrug << endl;
+	double pi = ((double)BrTuKrug / (double)BrTuKvad)*4;
+	cout << "4*BrTuKrug/BrTuKvad:  " << pi << endl;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
